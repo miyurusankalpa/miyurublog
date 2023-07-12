e3f7c4126cdafa3df68709378de09bfb25edf084
Miyuru Sankalpa <miyuru@miyuru.lk>
2023-07-12 13:05:10 +0530

    REMOVE DRAFT

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,7 +60,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    `gatsby-plugin-draft`,
+   // `gatsby-plugin-draft`,
     `gatsby-plugin-sitemap`,
     {
       resolve: "gatsby-plugin-matomo",
